57913f1567b437396d028bcc1001ef116c560b6d
YONGHUNI <dydgns0556@gmail.com>
2026-01-11 19:01:23 +0100

    fix: renv conflict

--- config.R
+++ config.R
@@ -2,6 +2,9 @@
 #  APP_NAME: ${{ secrets.APP_NAME }}
 #run: Rscript --verbose ./config.R
 
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+
 if (file.exists(".Renviron")) {
     
     readRenviron(".Renviron")
